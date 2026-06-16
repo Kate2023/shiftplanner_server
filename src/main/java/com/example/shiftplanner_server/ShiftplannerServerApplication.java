ea037ec6beda7e375ff7ded776348006bb91b393
Kate <Kate@KATE-LAPTOP-HP>
2026-06-16 23:16:49 +1200

    init

---
+++ src/main/java/com/example/shiftplanner_server/ShiftplannerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.shiftplanner_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftplannerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftplannerServerApplication.class, args);
+    }
+
+}
